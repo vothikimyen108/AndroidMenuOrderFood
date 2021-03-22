6917f515e1508dae2e20cab82456cd7162a8f890
vothikimyen108 <vothikinyen2k@gmail.com>
2021-03-22 22:21:21 +0700

    'desginui'

---
+++ app/src/main/java/com/example/vothikimyen_menufood/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.vothikimyen_menufood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
